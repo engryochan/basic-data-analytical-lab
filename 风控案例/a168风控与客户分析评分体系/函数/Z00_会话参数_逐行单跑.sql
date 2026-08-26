14e505dfb6335d3ddb69c9e33f92fb8b1e103e00
Ryochan Eng <engryochan@gmail.com>
2026-08-26 09:34:50 +0400

    a168_SQL总包_v12_0_0_HF4_*. csv 出炉
    - a168_SQL总包_v12_0_0_HF4_分批作业版_六层商业版.sql
    - a168_SQL总包_v12_0_0_HF4_原版审计版_六层商业版.sql

---
+++ 风控案例/a168风控与客户分析评分体系/函数/Z00_会话参数_逐行单跑.sql
@@ -0,0 +1,8 @@
+SET SESSION query_timeout = 259200;
+SET enable_spill = true;
+SET spill_mode = 'auto';
+SET cbo_cte_reuse = true;
+SET cbo_cte_reuse_rate = 1.0;
+SET enable_pipeline_engine = true;
+SET pipeline_dop = 0;
+SET enable_global_runtime_filter = true;
